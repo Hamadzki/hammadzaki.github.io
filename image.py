10526d07f74b804baaeedd7b206b47930cf1a810
hamadzki <hammadzaki1947@gmail.com>
2024-09-20 09:28:58 +0530

    Add: added crypto projects details

--- image.py
+++ image.py
@@ -63,6 +63,34 @@ def convert_specific_images_to_webp(folder_path, image_names, quality=60):
         img.save(webp_path, 'webp', quality=quality)
         print(f'Converted {image_name} to {webp_filename} with quality {quality}.')
 
+
+def optimize_specific_webp_images(image_paths, quality=40):
+    for img_path in image_paths:
+        # Check if the file is a webp file
+        if img_path.endswith('.webp'):
+            # Get original file size
+            original_size_kb = os.path.getsize(img_path) / 1024
+            
+            img = Image.open(img_path)
+
+            # Save the image with reduced quality
+            img.save(img_path, 'webp', quality=quality)
+
+            # Get new file size
+            new_size_kb = os.path.getsize(img_path) / 1024
+            
+            print(f'Reduced size of {os.path.basename(img_path)}:')
+            print(f'  Original size: {original_size_kb:.2f} KB')
+            print(f'  New size: {new_size_kb:.2f} KB')
+            print(f'  Quality set to: {quality}\n')
+        else:
+            print(f'Skipped {os.path.basename(img_path)}: Not a WebP file.')
+
+# Example usage:
+# image_paths = ['path/to/image1.webp', 'path/to/image2.webp']
+# optimize_webp_images(image_paths, quality=70)
+
+
 # Example usage:
 # For converting all images
 # convert_images_to_webp('assets/img/certificates')
@@ -70,7 +98,11 @@ def convert_specific_images_to_webp(folder_path, image_names, quality=60):
 
 # convert_images_to_webp('assets/img')
 
-optimize_webp_images('assets/img/project')
+# optimize_webp_images('assets/img/project')
 
 # For converting specific images
 # convert_specific_images_to_webp('assets/img/skills', ['airflow.png'])
+
+# images_path = ['portfolio/assets/img/project/crypto.webp','portfolio/assets/img/project/temperature.webp']
+images_path = ['portfolio/assets/img/project/crypto.webp']
+optimize_specific_webp_images(images_path)
